5cfb7b5de606f0a52271f72e2f70acccbf24ec75
samjau <sjau@sjau-mbp.corp.dropbox.com>
2014-08-11 09:11:08 -0700

    initial

---
+++ SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  Fakebox
+//
+//  Created by Sam Jau on 8/10/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var settingsScrollView: UIScrollView!
+    @IBOutlet weak var settingsImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingsScrollView.contentSize = settingsImageView.frame.size
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
